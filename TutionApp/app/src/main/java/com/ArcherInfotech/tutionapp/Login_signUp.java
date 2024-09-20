e1db60df5622c903e24c0221bd9cf66501c73150
TheSwastikLokhande <swastiklokhande2762003@gmail.com>
2024-09-20 11:29:28 +0530

    first commit

--- TutionApp/app/src/main/java/com/ArcherInfotech/tutionapp/Login_signUp.java
+++ TutionApp/app/src/main/java/com/ArcherInfotech/tutionapp/Login_signUp.java
@@ -8,13 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Login_signUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_sign_up);
+
 
     }
 }
--- TutionApp/app/src/main/java/com/ArcherInfotech/tutionapp/SplashScreen.java
+++ TutionApp/app/src/main/java/com/ArcherInfotech/tutionapp/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.ArcherInfotech.tutionapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, Login_signUp.class));
+            }
+        }, 2000);
+
 
     }
 }
