bbc13b66842e29919c406b69aedb94f2ba0724a9
poojaapatil2117 <poojalpatil21@gmail.com>
2024-09-24 17:23:35 +0530

    Cource List And Cource Details

---
+++ TutionApp/app/src/main/java/com/ArcherInfotech/tutionapp/Adaptor/CourceAdaptor.java
@@ -0,0 +1,90 @@
+package com.ArcherInfotech.tutionapp.Adaptor;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ArcherInfotech.tutionapp.Listener.ItemClickListner;
+import com.ArcherInfotech.tutionapp.Modal.Cources;
+import com.ArcherInfotech.tutionapp.R;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class CourceAdaptor extends RecyclerView.Adapter<CourceAdaptor.CourceViewHolder>{
+
+    List<Cources> courcesList;
+    static ItemClickListner clickListner;
+
+    public void setClickListner(ItemClickListner clickListner) {
+        CourceAdaptor.clickListner = clickListner;
+    }
+
+
+    public CourceAdaptor(List<Cources> courcesList)
+    {
+        this.courcesList=courcesList;
+    }
+
+    @NonNull
+    @Override
+    public CourceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cource_cards,parent,false);
+        return new CourceViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourceViewHolder holder, int position) {
+
+        Cources cource =courcesList.get(position);
+
+        holder.courcenm.setText(cource.getCourceName());
+        holder.duration.setText(cource.getDuration());
+        holder.rating.setImageResource(cource.getRating());
+        holder.bg.setImageResource(cource.getBgimg());
+        holder.courimg.setImageResource(cource.getMaincourceimg());
+    }
+
+    @Override
+    public int getItemCount() {
+
+        if(courcesList != null)
+        {
+            return courcesList.size();
+        }
+        return 0;
+    }
+
+    public static class CourceViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView courcenm;
+        TextView duration;
+        ImageView rating;
+        ImageView bg;
+        ImageView courimg;
+
+        public CourceViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            courcenm = itemView.findViewById(R.id.courcename);
+            duration = itemView.findViewById(R.id.duration);
+            rating = itemView.findViewById(R.id.ratings);
+            bg = itemView.findViewById(R.id.courcebg);
+            courimg = itemView.findViewById(R.id.courceimg);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (clickListner != null)
+            {
+                clickListner.onClick(view,getAdapterPosition());
+            }
+        }
+    }
+}
---
+++ TutionApp/app/src/main/java/com/ArcherInfotech/tutionapp/Cource_Details.java
@@ -0,0 +1,112 @@
+package com.ArcherInfotech.tutionapp;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Cource_Details extends AppCompatActivity {
+
+    TextView courceTitle1,courceTitle2,courcepoint1,courcepoint2,courcepoint3,courcepoint4,courcepoint5,
+            courcepoint6,courcepoint7,courcepoint8,courcepoint9,courcepoint10;
+
+    TextView instructername,instruinto1,instruinto2,instruinto3,instruinto4,fees;
+
+    ImageView insProfImg;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cource_details);
+
+
+        //Cource name Id
+        courceTitle1 = findViewById(R.id.courcename1);
+        courceTitle2 = findViewById(R.id.courcename2);
+
+        //points Id
+        courcepoint1 = findViewById(R.id.point1);
+        courcepoint2 = findViewById(R.id.point2);
+        courcepoint3 = findViewById(R.id.point3);
+        courcepoint4 = findViewById(R.id.point4);
+        courcepoint5 = findViewById(R.id.point5);
+        courcepoint6 = findViewById(R.id.point6);
+        courcepoint7 = findViewById(R.id.point7);
+        courcepoint8 = findViewById(R.id.point8);
+        courcepoint9 = findViewById(R.id.point9);
+        courcepoint10 = findViewById(R.id.point10);
+
+        //instructors id
+        instructername = findViewById(R.id.instructername);
+        instruinto1 = findViewById(R.id.instructerinfo1);
+        instruinto2 = findViewById(R.id.instructerinfo2);
+        instruinto3 = findViewById(R.id.instructerinfo4);
+        instruinto4 = findViewById(R.id.instructerinfo4);
+
+        insProfImg = findViewById(R.id.instructerimg);
+
+
+        //fees
+        fees = findViewById(R.id.feestext);
+
+        //Coucres name
+        String titleOne = getIntent().getStringExtra("Cource_Name_One");
+        String titleTwo = getIntent().getStringExtra("Cource_Name_Two");
+
+
+        //Cources points
+        String coureP1 = getIntent().getStringExtra("Cource_Point_One");
+        String coureP2 = getIntent().getStringExtra("Cource_Point_Two");
+        String coureP3 = getIntent().getStringExtra("Cource_Point_Three");
+        String coureP4 = getIntent().getStringExtra("Cource_Point_Four");
+        String coureP5 = getIntent().getStringExtra("Cource_Point_Five");
+        String coureP6 = getIntent().getStringExtra("Cource_Point_Six");
+        String coureP7 = getIntent().getStringExtra("Cource_Point_Seven");
+        String coureP8 = getIntent().getStringExtra("Cource_Point_Eight");
+        String coureP9 = getIntent().getStringExtra("Cource_Point_Nine");
+        String coureP10 = getIntent().getStringExtra("Cource_Point_Ten");
+
+        //Instructer
+        String instructernm = getIntent().getStringExtra("Instructer_name");
+        String instructerdetail1 = getIntent().getStringExtra("Instructer_detail_one");
+        String instructerdetail2 = getIntent().getStringExtra("Instructer_detail_one");
+        String instructerdetail3 = getIntent().getStringExtra("Instructer_detail_one");
+        String instructerdetail4 = getIntent().getStringExtra("Instructer_name");
+
+        //fees
+        String courceFees = getIntent().getStringExtra("Cource_Fees");
+
+        //Coucre name
+        courceTitle1.setText(titleOne);
+        courceTitle2.setText(titleTwo);
+
+        //cource point
+        courcepoint1.setText(coureP1);
+        courcepoint2.setText(coureP2);
+        courcepoint3.setText(coureP3);
+        courcepoint4.setText(coureP4);
+        courcepoint5.setText(coureP5);
+        courcepoint6.setText(coureP6);
+        courcepoint7.setText(coureP7);
+        courcepoint8.setText(coureP8);
+        courcepoint9.setText(coureP9);
+        courcepoint10.setText(coureP10);
+
+        //Instructer
+        instructername.setText(instructernm);
+        instruinto1.setText(instructerdetail1);
+        instruinto2.setText(instructerdetail2);
+        instruinto3.setText(instructerdetail3);
+        instruinto4.setText(instructerdetail4);
+
+        //Fess
+        fees.setText(courceFees);
+    }
+}
